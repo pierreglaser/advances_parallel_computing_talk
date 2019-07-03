33c063e5662f94b8570e3260917d64170efa5d0a
Pierre Glaser <pierreglaser@msn.com>
2019-07-03 10:14:37 +0200

    snapshot

---
+++ scripts/psf_analysis_script.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+params = {
+        "axes.titlesize": 24,
+        "axes.labelsize": 20,
+        "lines.linewidth": 3,
+        "lines.markersize": 10,
+        "xtick.labelsize": 16,
+        "ytick.labelsize": 16,
+        "font.size": 16,
+}
+mpl.rcParams.update(params)
+
+
+df = pd.read_csv("python_psf_external_18.csv")
+python_usage = df[
+    [x for x in df.columns if "What do you use Python for?" in x and not
+     'Other' in x]
+]
+f, ax = plt.subplots(figsize=(11, 4.5))
+ax.set_xlim(11000, 0)
+ax.yaxis.set_ticks_position('right')
+
+python_usage[~python_usage.isnull()] = 1
+python_usage.columns = [x.split(':')[0] for x in python_usage.columns]
+sum_python_usage = python_usage.sum()
+top_5_python_usage = sum_python_usage.sort_values().iloc[-5:]
+top_5_python_usage.plot.barh(ax=ax, width=0.5)
+
+f.subplots_adjust(right=0.3)
+ax.xaxis.set_visible(False)
+for loc in ['right', 'left', 'top', 'bottom']:
+    ax.spines[loc].set_visible(False)
+
+i = 0
+for idx, row in top_5_python_usage.iteritems():
+    ax.text(row + 4300, i-0.1, '{0:.1%}'.format(row/len(python_usage)))
+    i += 1
+f.savefig('media/psf_usage_survey.png', dpi=f.dpi)
---
+++ scripts/test_script.py
@@ -0,0 +1,7 @@
+class A:
+    def __init__(self, e):
+        self.e = e
+        try:
+            pass
+        except Exception:
+            pass
